0ef2761c82cb18f6e23159ee6e4cb02a260981d7
Edward Ng <ehng359@ucla.edu>
2023-09-08 15:17:15 -0700

    Added tsconfig + fixed issue with ever-expanding innerHTML content on textarea props

--- grouping.js
+++ grouping.js
@@ -1,17 +1,63 @@
 var _a;
 // Load currently available items that have been saved through localStorage.
 // for (var i=0; i < ...; i++) loop through all of the sections
-var form = document.getElementsByTagName("form")[0];
+var form = document.getElementById("main");
 var local_index = localStorage.getItem("total");
 var index = local_index == null ? form.childElementCount : Number(local_index) - 1;
 if (index != null) {
     console.log("Init");
     var localContents = localStorage.getItem("formContents");
     if (localContents != null) {
-        localContents = localContents.slice(1, localContents.length - 2);
+        localContents = localContents.replace(/(\\|\")/g, "");
+        console.log(localContents);
         form.insertAdjacentHTML("beforeend", localContents);
     }
 }
+var delegateTextAreas = function () {
+    var textareas = document.getElementsByTagName("textarea");
+    for (var i = 0; i < textareas.length; i++) {
+        textareas[i].addEventListener("keydown", function (event) {
+            console.log(event.key);
+            var target = event.currentTarget;
+            switch (event.key) {
+                case "Control":
+                case "Enter":
+                case "Alt":
+                case "Meta":
+                    event.preventDefault();
+                    break;
+                case "Tab":
+                    break;
+                case "Backspace":
+                    var selectionBkSpc = window.getSelection();
+                    if (selectionBkSpc != null) {
+                        console.log("Selection backspace");
+                        console.log(selectionBkSpc.deleteFromDocument());
+                    }
+                    else {
+                        console.log("Simple deletion");
+                        target.innerHTML = target.innerHTML.slice(0, -1);
+                    }
+                    console.log(target);
+                    break;
+                default:
+                    event.preventDefault();
+                    var selectionDefault = document.getSelection();
+                    // selectionDefault != null
+                    if (false) {
+                        console.log(selectionDefault === null || selectionDefault === void 0 ? void 0 : selectionDefault.anchorOffset);
+                    }
+                    else {
+                        target.innerHTML = target.innerHTML + event.key;
+                        console.log(target);
+                    }
+            }
+            localSave();
+        });
+    }
+    return textareas;
+};
+var textarea = delegateTextAreas();
 function localSave() {
     localStorage.setItem("total", String(index ? index + 1 : 0));
     var formContents = form.innerHTML;
@@ -21,9 +67,11 @@ function localSave() {
 }
 (_a = document.getElementById("addGrouping")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", addGrouping);
 function addGrouping() {
-    var element = "\n        <label for=\"label_".concat(index, "\" name=\"grouping").concat(index, "\">Grouping ").concat(index, "</label>\n        <section id=\"label").concat(index, "\">\n            <textarea></textarea>\n            <textarea></textarea>\n        </section>\n    ");
+    var element = "\n        <label for=label".concat(index, " name=grouping").concat(index, ">Grouping ").concat(index, "</label>\n        <section id=label").concat(index, ">\n            <textarea id=key").concat(index, "></textarea>\n            <textarea id=value").concat(index, "></textarea>\n        </section>\n    ");
     form.insertAdjacentHTML("beforeend", element);
+    console.log(element);
     index += 1;
+    textarea = delegateTextAreas();
     localSave();
 }
 // Note: we can store items from previous session
--- grouping.ts
+++ grouping.ts
@@ -1,22 +1,67 @@
 // Load currently available items that have been saved through localStorage.
 // for (var i=0; i < ...; i++) loop through all of the sections
-let form = document.getElementsByTagName("form")[0]
+let form = document.getElementById("main")
 
 let local_index = localStorage.getItem("total")
-let index = local_index == null ? form.childElementCount : Number(local_index) - 1
+let index = local_index == null ? form!.childElementCount : Number(local_index) - 1
 
 if (index != null) {
     console.log("Init")
     let localContents = localStorage.getItem("formContents")
     if (localContents != null) {
-        localContents = localContents.slice(1, localContents.length - 2)
-        form.insertAdjacentHTML("beforeend", localContents)
+        localContents = localContents.replace(/(\\|\")/g, "")
+        form!.insertAdjacentHTML("beforeend", localContents)
     }
 }
 
+const delegateTextAreas = () => {
+    let textareas = document.getElementsByTagName("textarea")
+    for (let i = 0; i < textareas.length; i++) {
+        textareas[i].addEventListener("keydown", (event) => {
+            console.log(event.key)
+            let target = <HTMLElement> event.currentTarget
+            switch (event.key) {
+                case "Control":
+                case "Enter":
+                case "Alt":
+                case "Meta":
+                    event.preventDefault()
+                    break;
+                case "Tab":
+                    break;
+                case "Backspace":
+                    let selectionBkSpc = window.getSelection()
+                    if (selectionBkSpc != null) {
+                        console.log("Selection backspace")
+                        console.log(selectionBkSpc.deleteFromDocument())
+                    } else {
+                        console.log("Simple deletion")
+                        target.innerHTML = target.innerHTML.slice(0, -1)
+                    }
+                    console.log(target)
+                    break;
+                default:
+                    event.preventDefault()
+                    let selectionDefault = document.getSelection()
+                    // selectionDefault != null
+                    if (false) {
+                        console.log(selectionDefault?.anchorOffset)
+                    } else {
+                        target.innerHTML = target.innerHTML + event.key
+                        console.log(target)
+                    }
+            }
+            localSave()
+        })
+    }
+    return textareas
+}
+
+let textarea = delegateTextAreas()
+
 function localSave() : void {
     localStorage.setItem("total", String(index ? index + 1 : 0))
-    let formContents = form.innerHTML
+    let formContents = form!.innerHTML
     formContents = formContents.replace(/(\r\n|\n|\r)/gm, "")
     formContents = formContents.trim()
     localStorage.setItem("formContents", JSON.stringify(formContents))
@@ -26,16 +71,20 @@ document.getElementById("addGrouping")?.addEventListener("click", addGrouping)
 
 function addGrouping() : void {
     const element = `
-        <label for="label_${index}" name="grouping${index}">Grouping ${index}</label>
-        <section id="label${index}">
-            <textarea></textarea>
-            <textarea></textarea>
+        <label for=label${index} name=grouping${index}>Grouping ${index}</label>
+        <section id=label${index}>
+            <textarea id=key${index}></textarea>
+            <textarea id=value${index}></textarea>
         </section>
     `
-    form.insertAdjacentHTML("beforeend", element)
+    form!.insertAdjacentHTML("beforeend", element)
+    console.log(element)
     index += 1
+    textarea = delegateTextAreas()
     localSave()
 }
+
+
 
 // Note: we can store items from previous session
 // if (localStorage.getItem("key") == null) {
