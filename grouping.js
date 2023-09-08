b2c98c3c9f84d2d4ada831795ac3c57ce9124e96
Edward Ng <ehng359@ucla.edu>
2023-09-08 00:50:26 -0700

    Added capability to create multiple different appended groupings + localStorage framework

--- grouping.js
+++ grouping.js
@@ -1,5 +1,29 @@
-var _a;
-(_a = document.getElementById("hello")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", helloWorld);
-function helloWorld() {
-    console.log("hello world!");
+var _a, _b;
+// Load currently available items that have been saved through localStorage.
+// for (var i=0; i < ...; i++) loop through all of the sections
+var form = document.getElementsByTagName("form")[0];
+var index = form.childElementCount;
+// <label for="label_{count}" name="grouping{count}">Grouping {count}</label>
+// <section id="label{count}">
+//     <textarea></textarea>
+//     <textarea></textarea>
+// </section>
+(_a = document.getElementById("saveData")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", localSave);
+function localSave() {
+    var textBoxes = document.getElementsByTagName("textarea");
+    // console.log("hello world!")
+    // console.log(textBoxes)
 }
+(_b = document.getElementById("addGrouping")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", addGrouping);
+function addGrouping() {
+    var element = "\n        <label for=\"label_".concat(index, "\" name=\"grouping").concat(index, "\">Grouping ").concat(index, "</label>\n        <section id=\"label").concat(index, "\">\n            <textarea></textarea>\n            <textarea></textarea>\n        </section>\n    ");
+    form.insertAdjacentHTML("beforeend", element);
+    index += 1;
+}
+// Note: we can store items from previous session
+// if (localStorage.getItem("key") == null) {
+//     localStorage.setItem("key", "string")
+// } else {
+//     console.log("Key is already stored from previous session")
+// }
+// On button click, get the total amount of elements w
--- grouping.ts
+++ grouping.ts
@@ -1,6 +1,38 @@
+// Load currently available items that have been saved through localStorage.
+// for (var i=0; i < ...; i++) loop through all of the sections
+let form = document.getElementsByTagName("form")[0]
+let index = form.childElementCount
+// <label for="label_{count}" name="grouping{count}">Grouping {count}</label>
+// <section id="label{count}">
+//     <textarea></textarea>
+//     <textarea></textarea>
+// </section>
 
-document.getElementById("hello")?.addEventListener("click", helloWorld)
-
-function helloWorld() : void {
-    console.log("hello world!")
+document.getElementById("saveData")?.addEventListener("click", localSave)
+function localSave() : void {
+    let textBoxes = document.getElementsByTagName("textarea")
+    // console.log("hello world!")
+    // console.log(textBoxes)
 }
+
+document.getElementById("addGrouping")?.addEventListener("click", addGrouping)
+
+function addGrouping() : void {
+    const element = `
+        <label for="label_${index}" name="grouping${index}">Grouping ${index}</label>
+        <section id="label${index}">
+            <textarea></textarea>
+            <textarea></textarea>
+        </section>
+    `
+    form.insertAdjacentHTML("beforeend", element)
+    index += 1
+}
+
+// Note: we can store items from previous session
+// if (localStorage.getItem("key") == null) {
+//     localStorage.setItem("key", "string")
+// } else {
+//     console.log("Key is already stored from previous session")
+// }
+// On button click, get the total amount of elements w
