4a9634028a75a6c024ccf93661a18cad90623728
Edward Ng <ehng359@ucla.edu>
2023-09-08 11:38:21 -0700

    All value-pair boxes persist; values inputted into text fields do not

--- grouping.js
+++ grouping.js
@@ -1,24 +1,30 @@
-var _a, _b;
+var _a;
 // Load currently available items that have been saved through localStorage.
 // for (var i=0; i < ...; i++) loop through all of the sections
 var form = document.getElementsByTagName("form")[0];
-var index = form.childElementCount;
-// <label for="label_{count}" name="grouping{count}">Grouping {count}</label>
-// <section id="label{count}">
-//     <textarea></textarea>
-//     <textarea></textarea>
-// </section>
-(_a = document.getElementById("saveData")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", localSave);
-function localSave() {
-    var textBoxes = document.getElementsByTagName("textarea");
-    // console.log("hello world!")
-    // console.log(textBoxes)
+var local_index = localStorage.getItem("total");
+var index = local_index == null ? form.childElementCount : Number(local_index) - 1;
+if (index != null) {
+    console.log("Init");
+    var localContents = localStorage.getItem("formContents");
+    if (localContents != null) {
+        localContents = localContents.slice(1, localContents.length - 2);
+        form.insertAdjacentHTML("beforeend", localContents);
+    }
 }
-(_b = document.getElementById("addGrouping")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", addGrouping);
+function localSave() {
+    localStorage.setItem("total", String(index ? index + 1 : 0));
+    var formContents = form.innerHTML;
+    formContents = formContents.replace(/(\r\n|\n|\r)/gm, "");
+    formContents = formContents.trim();
+    localStorage.setItem("formContents", JSON.stringify(formContents));
+}
+(_a = document.getElementById("addGrouping")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", addGrouping);
 function addGrouping() {
     var element = "\n        <label for=\"label_".concat(index, "\" name=\"grouping").concat(index, "\">Grouping ").concat(index, "</label>\n        <section id=\"label").concat(index, "\">\n            <textarea></textarea>\n            <textarea></textarea>\n        </section>\n    ");
     form.insertAdjacentHTML("beforeend", element);
     index += 1;
+    localSave();
 }
 // Note: we can store items from previous session
 // if (localStorage.getItem("key") == null) {
@@ -27,3 +33,4 @@ function addGrouping() {
 //     console.log("Key is already stored from previous session")
 // }
 // On button click, get the total amount of elements w
+// export {};
--- grouping.ts
+++ grouping.ts
@@ -1,18 +1,25 @@
 // Load currently available items that have been saved through localStorage.
 // for (var i=0; i < ...; i++) loop through all of the sections
 let form = document.getElementsByTagName("form")[0]
-let index = form.childElementCount
-// <label for="label_{count}" name="grouping{count}">Grouping {count}</label>
-// <section id="label{count}">
-//     <textarea></textarea>
-//     <textarea></textarea>
-// </section>
 
-document.getElementById("saveData")?.addEventListener("click", localSave)
+let local_index = localStorage.getItem("total")
+let index = local_index == null ? form.childElementCount : Number(local_index) - 1
+
+if (index != null) {
+    console.log("Init")
+    let localContents = localStorage.getItem("formContents")
+    if (localContents != null) {
+        localContents = localContents.slice(1, localContents.length - 2)
+        form.insertAdjacentHTML("beforeend", localContents)
+    }
+}
+
 function localSave() : void {
-    let textBoxes = document.getElementsByTagName("textarea")
-    // console.log("hello world!")
-    // console.log(textBoxes)
+    localStorage.setItem("total", String(index ? index + 1 : 0))
+    let formContents = form.innerHTML
+    formContents = formContents.replace(/(\r\n|\n|\r)/gm, "")
+    formContents = formContents.trim()
+    localStorage.setItem("formContents", JSON.stringify(formContents))
 }
 
 document.getElementById("addGrouping")?.addEventListener("click", addGrouping)
@@ -27,6 +34,7 @@ function addGrouping() : void {
     `
     form.insertAdjacentHTML("beforeend", element)
     index += 1
+    localSave()
 }
 
 // Note: we can store items from previous session
@@ -37,4 +45,4 @@ function addGrouping() : void {
 // }
 // On button click, get the total amount of elements w
 
-export {};
+// export {};
